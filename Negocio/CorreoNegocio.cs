169f395ec3a428bea5ef32a58183e1a73b07bd67
Jonatan Jorge Adrián Bacqué <jonatanbacque@hotmail.com>
2020-12-10 18:01:12 -0300

    Correo 2

--- Negocio/CorreoNegocio.cs
+++ Negocio/CorreoNegocio.cs
@@ -11,6 +11,29 @@ namespace Negocio
     {
         private SmtpClient cliente;
         private MailMessage email;
+        public string encabezado = "<html lang=es>" +
+                        "<head>" +
+                        "<link rel=stylesheet href=https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css >" +
+                        "</head>" +
+                        "<body>" +
+                        "<table align=center>" +
+                        "<tr>" +
+                        "<td>" +
+                        "<div align=center>" +
+                        "<img src=https://ss-static-01.esmsv.com/id/117805/galeriaimagenes/obtenerimagen/?width=75&height=75&id=sitio_logo&ultimaModificacion=2020-11-14+07%3A28%3A53 >" +
+                        "</div>" +
+                        "<h1> Aleli Esencias </h1>";
+        public string firma = "<hr/>" +
+                        "<span> Agradecemos su visita y le enviamos saludos cordiales.</span>" +
+                        "</td>" +
+                        "</tr>" +
+                        "</table>" +
+                        "<script src = https://code.jquery.com/jquery-3.5.1.min.js ></script>" +
+                        "<script src = https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js ></script>" +
+                        "<script src = https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js ></script>" +
+                        "</body>" +
+                        "</html>";
+
         void conectar()
         {
             cliente = new SmtpClient("smtp.gmail.com", 587);
@@ -26,32 +49,14 @@ namespace Negocio
                 if (usuario != null)
                 {
                     conectar();
-                    string body ="<html lang=es>" +
-                        "<head>" +
-                        "<link rel=stylesheet href=https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css >" +
-                        "</head>" +
-                        "<body>" +
-                        "<table align=center>" +
-                        "<tr>" +
-                        "<td>" +
-                        "<div align=center>" +
-                        "<img src=https://ss-static-01.esmsv.com/id/117805/galeriaimagenes/obtenerimagen/?width=75&height=75&id=sitio_logo&ultimaModificacion=2020-11-14+07%3A28%3A53 >" +
-                        "</div>" +
-                        "<h1> Bienvenido al eCommerce de Aleli Esencias </h1>" +
-                        "<h5> Estimado " + usuario.persona.Apellido + " " + usuario.persona.Nombre + " recibimos su registro.</h5>" +
+                    string body = encabezado +
+                        "<h5> Estimad@ " + usuario.persona.Apellido + " " + usuario.persona.Nombre + " recibimos su registro.</h5>" +
+                        "<br/>" +
                         "<h6> Su usuario es " + usuario.Nombre + ".</h6>" +
+                        "<br/>" +
                         "<h6> Su password es " + usuario.Password + ".</h6>" +
-                        "<h6> Esperamos tenga una grata experiencia de compra.</h6>" +
-                        "<hr/" +
-                        "<span> Agradecemos su visita y le enviamos saludos cordiales.</span>" +
-                        "</td>" +
-                        "</tr>" +
-                        "</table>" +
-                        "<script src = https://code.jquery.com/jquery-3.5.1.min.js ></script>" +
-                        "<script src = https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js ></script>" +
-                        "<script src = https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js ></script>" +
-                        "</body>" +
-                        "</html>";
+                        "<br/>" +
+                        "<h6> Esperamos tenga una grata experiencia de compra.</h6>" + firma;
 
                     email = new MailMessage();
                     email.From = new MailAddress("jonatanbacque@gmail.com", "Aleli Esencias");
